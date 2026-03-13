2eb519d618f26036f81ffd9910dfcfb6d5d30292
Ubuntu <ubuntu@ip-172-31-107-35.ap-southeast-2.compute.internal>
2026-03-13 08:15:08 +0000

    apiRouter created

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+// Put your code here
+const apiRouter = require("./src/routes/index");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 3000;
+
+// Put your code here
+
+app.use("/api", apiRouter);
+
+app.get("/", (req, res) => {
+    res.send("Hello, IFN666!");
+  });
+
+app.listen(port, () => {
+  console.log(`Server is listening on :${port}`)
+});
+
+module.exports = app;
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const taxonRouter = require("./taxon");
+
+router.use("/taxons", taxonRouter);
+
+module.exports = router;
